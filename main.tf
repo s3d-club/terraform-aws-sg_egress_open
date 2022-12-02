14482340fac1757ef606db2446f067f374e576c9
Mark Farnsworth <farnsworth2008@gmail.com>
2022-12-02 21:53:25 +0000

    1.0.0 Created first release

--- main.tf
+++ main.tf
@@ -1,18 +1,23 @@
-data "aws_vpc" "this" {
-  default = true
+module "name" {
+  source = "github.com/s3d-club/terraform-external-name?ref=v1.0.1"
+
+  path    = path.module
+  context = join("-", [var.name_prefix, "egress-open"])
+  tags    = var.tags
 }
 
 resource "aws_security_group" "this" {
-  name_prefix = "unrestricted-egress-"
-  description = "Unrestricted Egress"
-  vpc_id      = data.aws_vpc.this.id
+  name_prefix = module.name.prefix
+  description = "Egress OPEN for all protocols"
+  tags        = module.name.tags
+  vpc_id      = var.vpc_id
 
   egress {
-    description      = "Unrestricted Egress"
+    description      = "Egress for all ports/protocols"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = var.cidr_blocks
-    ipv6_cidr_blocks = var.ipv6_cidr_blocks
+    cidr_blocks      = var.cidrs
+    ipv6_cidr_blocks = var.cidr6s
   }
 }
